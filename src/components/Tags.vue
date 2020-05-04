4089bca005c60a93da39d811aa306cade6f13226
weizhexin <1315685142@qq.com>
2020-05-04 17:53:57 +0800

    money分开成模块化

---
+++ src/components/Tags.vue
@@ -0,0 +1,51 @@
+<template>
+     <div class="tags">
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+      </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:"Tags"
+    }
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+    > li {
+      background: rgb(217, 217, 217);
+      $h: 24px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+    }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 6px;
+    }
+  }
+}
+</style>
