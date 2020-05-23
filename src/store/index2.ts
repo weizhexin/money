b45c2d3d8d8b0634eee77fd03182f13afbc74b6f
weizhexin <1315685142@qq.com>
2020-05-23 17:23:51 +0800

    模块化store删除了model

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,31 +1,9 @@
-import tagListModel from '@/models/tagListModel'
-import recordListModels from '@/models/recordListModels'
+import recordStore from './recordStore';
+import tagStore from './tagStore';
 const store = {
     //record store
-    recordList :recordListModels.fetch(),
-    createRecord :(record: RecordItem)=>{
-      return recordListModels.create(record);
-    },
-    
-    
+    ...recordStore,
     //tag store
-    removeTag : (id: string)=>{
-        return tagListModel.remove(id);
-    },
-    updateTag : (id: string, name: string)=>{
-      return tagListModel.update(id, name);
-    },
-    findTag (id: string){
-      return this.tagList.filter(t=>t.id === id)[0];
-    },
-    tagList : tagListModel.fetch(),
-    createTag : (name: string)=>{
-      const message =  tagListModel.create(name);
-      if(message == 'duplicated'){
-          window.alert('不能重复名称哦！亲')
-      }else if(message == 'success'){
-          window.alert('添加标签成功')
-      }
-    }
+   ...tagStore
     }
     export default store;
