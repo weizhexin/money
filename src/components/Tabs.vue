a4c9574fd4efd23fd8be3bccaa2a96eee612eedd
weizhexin <1315685142@qq.com>
2020-05-31 15:39:54 +0800

    完成statistics.vue样式

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,7 +1,6 @@
 <template>
-
-       <ul class="tabs">
-          <li v-for="item in dataSource" :key="item.value"
+       <ul class="tabs" :class="{[classPrefix + '-tabs']: classPrefix}">
+          <li v-for="item in dataSource" :key="item.value" class="tabs-item"
           :class="{selected: item.value === value, [classPrefix + '-tabs-item']:classPrefix}"
           @click="select(item)">{{item.text}}</li> 
         </ul>
@@ -31,7 +30,7 @@
     display: flex;
     text-align: center;
     font-size: 24px;
-      > li {
+    &-item {
       width: 50%;
       height: 64px;
       display: flex;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,21 +2,54 @@
     <Layout>
       <Tabs class-prefix="type" :data-source = "recordTypeList" :value.sync = 'type' />
       <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" />  
+      <ol>
+        <li v-for="(group,index) in result" :key="index">
+          <h3 class="title">{{group.title}}</h3>
+          <ol>
+            <li v-for="item in group.items" :key="item.id" class="record">
+              <span>{{tagString(item.tags)}}</span>
+              <span class="notes">{{item.notes}}</span>
+              <span>￥{{item.amount}}</span> 
+            </li>
+          </ol>
+        </li>
+      </ol>   
+      
     </Layout> 
 </template>
 
 <script lang = "ts">
 import Vue from 'vue';
-import Types from '@/components/Types.vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constants/intervalList'
 import recordTypeList from '@/constants/recordTypeList'
 
 @Component({
-  components:{Types,Tabs}
+  components:{Tabs}
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]){
+    return tags.length === 0? '无' : tags.join(',');
+  }
+  get recordList(){
+    return (this.$store.state as RootState).recordList;
+  }
+  get result(){
+    const {recordList} = this;
+    type HasTableValue = {title: string; items: RecordItem[] }
+    const hashTable: {[key: string]: HasTableValue} = {};
+    for(let i = 0; i< recordList.length;i++){
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title:date, items:[]};
+      hashTable[date].items.push(recordList[i]);
+    }
+    console.log(hashTable)
+    return hashTable;
+  }
+  beforeCreate(){
+    this.$store.commit('fetchRecords')
+  }
  type = '-';
  interval = "day";
  intervalList = intervalList;
@@ -25,7 +58,27 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep .type-tabs-item{
+%item{
+  padding:8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.title{
+  @extend %item;
+}
+.record{
+  @extend %item;
+  background: white;
+}
+.notes{
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
+  ::v-deep {
+    .type-tabs-item{
     background: white;
     &.selected{
       background: #C4C4C4;
@@ -34,4 +87,9 @@ export default class Statistics extends Vue {
       }
     }
   }
+   .interval-tabs-item{
+     height:48px;
+   }
+  }
+  
 </style>
