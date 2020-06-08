4b8c12f251f1bfb178d8972cab080cd0cf186864
weizhexin <1315685142@qq.com>
2020-06-08 21:02:07 +0800

    3

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import Nav from '@/components/Nav.vue'
-import Layout from '@/components/Layout.vue'
-import Icon from '@/components/Icon.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Nav from '@/components/Nav.vue';
+import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
-Vue.component('Nav', Nav)
-Vue.component('Layout',Layout)
-Vue.component('Icon',Icon)
-
-
-
+Vue.component('Nav', Nav);
+Vue.component('Layout', Layout);
+Vue.component('Icon', Icon);
 
 new Vue({
-  router:router,
+  router,
   store,
   render: h => h(App)
 }).$mount('#app');
 
-window.onload = function(){
-  setTimeout(function(){
-    window.scrollTo(0,1000)
-  },0)
-}
-export default{
-  data(){
-    return {
-      publicPath: process.env.BASE_URL
-    }
-  }
-}
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 200);
+  }, 0);
+};
 
-if(document.documentElement.clientWidth > 500){
- // window.alert('为保证效果更佳，请使用手机浏览');
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开本页面，以保证浏览效果');
   const img = document.createElement('img');
-  img.src = "`${publicPath}qrcode.png`";
+  img.src = './qrcode.png';
   img.style.position = 'fixed';
-  img.style.top = '50%';
   img.style.left = '50%';
-  img.style.transform = 'translate(-50%, -50%)';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
   img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
   document.body.appendChild(img);
-
 }
